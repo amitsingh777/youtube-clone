38ce1b1ae2c23397e009a3b2312466fa7c6f1055
invincibleamitsingh <invincibleamitsingh@gmail.com>
2020-04-02 22:04:55 +0530

    Updates

---
+++ precache-manifest.425bd4e7bfcb5b411fe65e9bb218cc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a997fd73be75242bd5d94bcb8070f64",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "7a1ecf8c893b2c6f1b5b",
+    "url": "/youtube-clone/static/css/main.99781679.chunk.css"
+  },
+  {
+    "revision": "a03056814ca1c626378b",
+    "url": "/youtube-clone/static/js/2.2c8fed30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone/static/js/2.2c8fed30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1ecf8c893b2c6f1b5b",
+    "url": "/youtube-clone/static/js/main.58ba4d73.chunk.js"
+  },
+  {
+    "revision": "21fb8a625e1671ee320e",
+    "url": "/youtube-clone/static/js/runtime-main.b6cd16d8.js"
+  }
+]);
